584ae70883111a35c2e8d842f97711bed3d6f1c9
KieranMueller <kieran98mueller@gmail.com>
2023-08-07 22:31:54 -0500

    initial commit

---
+++ src/test/java/com/kieran/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kieran.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
