7c325c0c55646637f3e09a6a31336b9009ff8d1b
KieranMueller <kieran98mueller@gmail.com>
2023-08-07 23:23:38 -0500

    progress with JwtAuthenticationFilter

---
+++ src/main/java/com/kieran/security/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.kieran.security.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
